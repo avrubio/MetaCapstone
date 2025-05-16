3abbe9b94509323d05a2fd4265bbc6da52373207
avrubio <ariadna.vanegas@gmail.com>
2025-05-16 19:54:51 -0400

    created highlights section component and set up reusable card component

---
+++ src/components/Highlights/Card.tsx
@@ -0,0 +1,25 @@
+import Button from '../Button';
+
+type CardProps = {
+  title: string;
+  price: string;
+  description: string;
+  img: string;
+  button?: string;
+};
+
+const Card = ({ title, price, img, description, button }: CardProps) => {
+  return (
+    <>
+      <div className="card">
+        <img src={img} alt="" />
+        <p>{price}</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        {button ? <Button label={button} /> : <></>}
+      </div>
+    </>
+  );
+};
+
+export default Card;
