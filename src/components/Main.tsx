5f3ee3273b5002fcde65caee97cfc10ecc6087ef
avrubio <ariadna.vanegas@gmail.com>
2025-05-16 19:25:38 -0400

    set up semantic html for hero section in Main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,20 @@
+import Button from './Button';
+
+const Main = () => {
+  return (
+    <>
+      <main>
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          aliquam unde porro fugit, expedita in fuga dolorem nobis qui ut
+          numquam at, error nulla accusantium beatae ipsa neque sequi nostrum.
+        </p>
+        <Button label="Reserve a table" />
+      </main>
+    </>
+  );
+};
+
+export default Main;
